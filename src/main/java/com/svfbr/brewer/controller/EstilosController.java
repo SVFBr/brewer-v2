5bb1b055010cf2df80a279663d0e7c97b058a6ca
svfbr <sheilafreire.br@gmail.com>
2018-04-23 12:40:43 -0300

    11.4. Tratando exceções com ExceptionHandler

--- src/main/java/com/svfbr/brewer/controller/EstilosController.java
+++ src/main/java/com/svfbr/brewer/controller/EstilosController.java
@@ -35,14 +35,12 @@ public class EstilosController {
 		if (result.hasErrors()) {
 			return novo(estilo);
 		}
-
 		try {
 			cadastroEstiloService.salvar(estilo);
 		} catch (NomeEstiloJaCadastradoException e) {
 			result.rejectValue("nome", e.getMessage(), e.getMessage());
 			return novo(estilo);
 		}
-
 		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso");
 		return new ModelAndView("redirect:/estilos/novo");
 	}
@@ -52,11 +50,7 @@ public class EstilosController {
 		if (result.hasErrors()) {
 			return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
 		}
-		try {
-			estilo = cadastroEstiloService.salvar(estilo);
-		} catch (NomeEstiloJaCadastradoException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+		estilo = cadastroEstiloService.salvar(estilo);
 		return ResponseEntity.ok(estilo);
 	}
 
