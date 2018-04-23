6558fa7691205cebc20a69f335a265f1d39226af
svfbr <sheilafreire.br@gmail.com>
2018-04-22 22:59:35 -0300

    11.3. Melhorando mapeamento controller

--- src/main/java/com/svfbr/brewer/controller/CervejasController.java
+++ src/main/java/com/svfbr/brewer/controller/CervejasController.java
@@ -18,6 +18,7 @@ import com.svfbr.brewer.repository.Estilos;
 import com.svfbr.brewer.service.CadastroCervejaService;
 
 @Controller
+@RequestMapping("/cervejas")
 public class CervejasController {
 	
 	@Autowired
@@ -26,7 +27,7 @@ public class CervejasController {
 	@Autowired
 	private CadastroCervejaService cadastroCervejaService;
 
-	@RequestMapping("/cervejas/novo")
+	@RequestMapping("/novo")
 	public ModelAndView novo(Cerveja cerveja) {
 		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
 		mv.addObject("sabores", Sabor.values());
@@ -35,7 +36,7 @@ public class CervejasController {
 		return mv;
 	}
 	
-	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(cerveja);
